ac0dccd96d06a263d614c3819bb9b21c4526a417
kent.ruffle.mgj626 <kent.ruffle.mgj626@gmail.com>
2013-03-07 16:21:47 +0000

    バージョン1.1 $yを使用可能に テンプレートを3つ使用可能に 「共有後に終了する」オプションを追加

--- NowPlayingMusicExtension/src/jp/nita/NowPlayingMusicExtension/ExtensionActivity.java
+++ NowPlayingMusicExtension/src/jp/nita/NowPlayingMusicExtension/ExtensionActivity.java
@@ -37,10 +37,25 @@ import android.widget.TextView;
 
 public class ExtensionActivity extends Activity implements OnClickListener {
 	private static final String PREF_KEY = "NowPlayingMusicExtension";  
-	private static final String KEY_TEXT = "templete";  
+	private static final String KEY_TEXT_1 = "templete";
+	private static final String KEY_TEXT_2 = "templete2";
+	private static final String KEY_TEXT_3 = "templete3";
+	private static final String KEY_TEXT_QUIT = "quitAfterSharing"; 
 	
 	Uri trackUri;
 	String uri;
+	
+	String title;
+	String artist;
+	String album;
+	String duration;
+	String composer;
+	String year;
+	
+	String template1;
+	String template2;
+	String template3;
+	boolean quitAfterSharing;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -57,7 +72,7 @@ public class ExtensionActivity extends Activity implements OnClickListener {
 		initForm();
 
 		findViewById(R.id.tweet).setOnClickListener(this);
-		findViewById(R.id.edit).setOnClickListener(this);
+		findViewById(R.id.apply).setOnClickListener(this);
 		findViewById(R.id.share).setOnClickListener(this);
 		findViewById(R.id.cancel).setOnClickListener(this);
 
@@ -75,6 +90,7 @@ public class ExtensionActivity extends Activity implements OnClickListener {
 				new String[] {
 						MediaStore.Audio.Media.TITLE, MediaStore.Audio.Media.ARTIST,
 						MediaStore.Audio.Media.ALBUM, MediaStore.Audio.Media.DURATION,
+						MediaStore.Audio.Media.COMPOSER, MediaStore.Audio.Media.YEAR,
 						MediaStore.Audio.Media.DATA
 				}, null, null, null);
 
@@ -85,18 +101,50 @@ public class ExtensionActivity extends Activity implements OnClickListener {
 					// And retrieve the wanted information
 					String tweetContent;
 					SharedPreferences pref=getSharedPreferences(PREF_KEY,Activity.MODE_PRIVATE);
-					tweetContent=pref.getString(KEY_TEXT,getString(R.string.content_default));
+					
+					template1=pref.getString(KEY_TEXT_1,getString(R.string.content_default));
+					template2=pref.getString(KEY_TEXT_2,getString(R.string.content_default_2));
+					template3=pref.getString(KEY_TEXT_3,getString(R.string.content_default_3));
+					quitAfterSharing=pref.getBoolean(KEY_TEXT_QUIT,false);
+					
+					tweetContent=pref.getString(KEY_TEXT_1,getString(R.string.content_default));
 
 					Time time = new Time();
 	                time.set(trackCursor.getLong(trackCursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DURATION)));
-	                String duration=time.format("%M:%S");
+	                duration=time.format("%M:%S");
 					
-					tweetContent=tweetContent.replace("$t",trackCursor.getString(trackCursor.getColumnIndexOrThrow(MediaStore.Audio.Media.TITLE)));
-					tweetContent=tweetContent.replace("$a",trackCursor.getString(trackCursor.getColumnIndexOrThrow(MediaStore.Audio.Media.ARTIST)));
-					tweetContent=tweetContent.replace("$l",trackCursor.getString(trackCursor.getColumnIndexOrThrow(MediaStore.Audio.Media.ALBUM)));
-					tweetContent=tweetContent.replace("$d",duration);
-					uri=trackCursor.getString(trackCursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DATA));
+	                try{
+	                	title=trackCursor.getString(trackCursor.getColumnIndexOrThrow(MediaStore.Audio.Media.TITLE));
+	                }finally{
+	                	if(title==null) title="";
+	                }
+	                try{
+	                	artist=trackCursor.getString(trackCursor.getColumnIndexOrThrow(MediaStore.Audio.Media.ARTIST));
+	                }finally{
+	                	if(artist==null) artist="";
+	                }
+	                try{
+	                	album=trackCursor.getString(trackCursor.getColumnIndexOrThrow(MediaStore.Audio.Media.ALBUM));
+	                }finally{
+	                	if(album==null) album="";
+	                }
+	                try{
+	                	uri=trackCursor.getString(trackCursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DATA));
+	                }finally{
+	                	if(uri==null) uri="";
+	                }
+	                try{
+	                	composer=trackCursor.getString(trackCursor.getColumnIndexOrThrow(MediaStore.Audio.Media.COMPOSER));
+	                }finally{
+	                	if(composer==null) composer="";
+	                }
+	                try{
+	                	year=trackCursor.getString(trackCursor.getColumnIndexOrThrow(MediaStore.Audio.Media.YEAR));
+	                }finally{
+	                	if(year==null) year="";
+	                }
 
+					tweetContent=applyTemplate(tweetContent);
 					((TextView)findViewById(R.id.textField)).setText(tweetContent);
 
 				}
@@ -104,6 +152,16 @@ public class ExtensionActivity extends Activity implements OnClickListener {
 				trackCursor.close();
 			}
 		}
+	}
+	
+	public String applyTemplate(String param){
+		param=param.replace("$t",title);
+		param=param.replace("$a",artist);
+		param=param.replace("$l",album);
+		param=param.replace("$d",duration);
+		//param=param.replace("$c",composer);
+		param=param.replace("$y",year);
+		return param;
 	}
 
 	@Override
@@ -123,14 +181,44 @@ public class ExtensionActivity extends Activity implements OnClickListener {
 				intent.setType("text/plain");
 				intent.putExtra(Intent.EXTRA_TEXT, ((TextView)findViewById(R.id.textField)).getText().toString());
 				startActivity(intent);
+				if(quitAfterSharing) finish();
 			} catch (Exception e) {
 				Log.d("ExampleExtensionActivity", "Error");
 				e.printStackTrace();
 			}
-		}else if(arg0==(View)findViewById(R.id.edit)){
-			Intent intent=new Intent(this,PreferencesActivity.class);
-			intent.setAction(Intent.ACTION_VIEW);
-			startActivity(intent);
+		}else if(arg0==(View)findViewById(R.id.apply)){
+			CharSequence list[]=new String[4];
+			SharedPreferences pref=getSharedPreferences(PREF_KEY,Activity.MODE_PRIVATE);
+			template1=pref.getString(KEY_TEXT_1,getString(R.string.content_default));
+			template2=pref.getString(KEY_TEXT_2,getString(R.string.content_default_2));
+			template3=pref.getString(KEY_TEXT_3,getString(R.string.content_default_3));
+			list[0]="1: "+applyTemplate(template1);
+			list[1]="2: "+applyTemplate(template2);
+			list[2]="3: "+applyTemplate(template3);
+			list[3]=getString(R.string.edit_template);
+			new AlertDialog.Builder(ExtensionActivity.this)
+			.setTitle(getString(R.string.apply_template))
+			.setItems(list,new DialogInterface.OnClickListener(){
+				@Override
+				public void onClick(DialogInterface arg0, int arg1) {
+					switch(arg1){
+					case 0:
+						((TextView)findViewById(R.id.textField)).setText(applyTemplate(template1));
+						break;
+					case 1:
+						((TextView)findViewById(R.id.textField)).setText(applyTemplate(template2));
+						break;
+					case 2:
+						((TextView)findViewById(R.id.textField)).setText(applyTemplate(template3));
+						break;
+					case 3:
+						Intent intent=new Intent(ExtensionActivity.this,PreferencesActivity.class);
+						intent.setAction(Intent.ACTION_VIEW);
+						startActivity(intent);
+						break;
+					}
+				}
+			}).show();
 		}else if(arg0==(View)findViewById(R.id.share)){
 			try {
 				Uri trackUri = getIntent().getData();
@@ -141,6 +229,7 @@ public class ExtensionActivity extends Activity implements OnClickListener {
 				intent.setType(type);
 				intent.putExtra(Intent.EXTRA_STREAM, trackUri);
 				startActivity(intent);
+				if(quitAfterSharing) finish();
 			} catch (Exception e) {
 				Log.d("ExampleExtensionActivity", "Error");
 				e.printStackTrace();
--- NowPlayingMusicExtension/src/jp/nita/NowPlayingMusicExtension/PreferencesActivity.java
+++ NowPlayingMusicExtension/src/jp/nita/NowPlayingMusicExtension/PreferencesActivity.java
@@ -2,15 +2,20 @@ package jp.nita.NowPlayingMusicExtension;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.EditText;
+import android.widget.*;
 
 public class PreferencesActivity extends Activity implements OnClickListener {
 	private static final String PREF_KEY = "NowPlayingMusicExtension";  
-	private static final String KEY_TEXT = "templete";  
+	private static final String KEY_TEXT_1 = "templete";  
+	private static final String KEY_TEXT_2 = "templete2";  
+	private static final String KEY_TEXT_3 = "templete3";  
+	private static final String KEY_TEXT_QUIT = "quitAfterSharing";  
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -23,29 +28,71 @@ public class PreferencesActivity extends Activity implements OnClickListener {
 	}
 	
 	@Override
+	public void onPause(){
+		super.onPause();
+		savePrefs();
+	}
+	
+	@Override
 	public void onResume(){
 		super.onResume();
 		initForm();
 	}
 	
-	public void initForm(){
-		String temp;
+	public void savePrefs(){
 		SharedPreferences pref=getSharedPreferences(PREF_KEY,Activity.MODE_PRIVATE);
-		temp=pref.getString(KEY_TEXT,getString(R.string.content_default));
+		Editor editor=pref.edit();
+		editor.putString(KEY_TEXT_1,((EditText)findViewById(R.id.templete1)).getText().toString());
+		editor.putString(KEY_TEXT_2,((EditText)findViewById(R.id.templete2)).getText().toString());
+		editor.putString(KEY_TEXT_3,((EditText)findViewById(R.id.templete3)).getText().toString());
+		editor.putBoolean(KEY_TEXT_QUIT,((CheckBox)findViewById(R.id.quit_after_sharing)).isChecked());
+		editor.commit();
+	}
+	
+	public void initForm(){
+		String temp1,temp2,temp3;
+		boolean quitAfterSharing;
+		SharedPreferences pref=getSharedPreferences(PREF_KEY,Activity.MODE_PRIVATE);
+		temp1=pref.getString(KEY_TEXT_1,getString(R.string.content_default));
+		temp2=pref.getString(KEY_TEXT_2,getString(R.string.content_default_2));
+		temp3=pref.getString(KEY_TEXT_3,getString(R.string.content_default_3));
+		quitAfterSharing=pref.getBoolean(KEY_TEXT_QUIT,false);
 		
-		((EditText)findViewById(R.id.templete)).setText(temp);
+		((EditText)findViewById(R.id.templete1)).setText(temp1);
+		((EditText)findViewById(R.id.templete2)).setText(temp2);
+		((EditText)findViewById(R.id.templete3)).setText(temp3);
+		
+		((CheckBox)findViewById(R.id.quit_after_sharing)).setChecked(quitAfterSharing);
 	}
 
 	@Override
 	public void onClick(View v) {
 		if(v==(View)findViewById(R.id.ok)){
-			SharedPreferences pref=getSharedPreferences(PREF_KEY,Activity.MODE_PRIVATE);
-			Editor editor=pref.edit();
-			editor.putString(KEY_TEXT,((EditText)findViewById(R.id.templete)).getText().toString());
-			editor.commit();
+			savePrefs();
 			finish();
 		}else if(v==(View)findViewById(R.id.revert)){
-			((EditText)findViewById(R.id.templete)).setText(getString(R.string.content_default));
+			AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+	        alertDialogBuilder.setTitle(getString(R.string.revert));
+	        alertDialogBuilder.setMessage(getString(R.string.confirm_revert));
+	        alertDialogBuilder.setPositiveButton(getString(R.string.ok),
+	                new DialogInterface.OnClickListener() {
+	                    @Override
+	                    public void onClick(DialogInterface dialog, int which) {
+	                    	((EditText)findViewById(R.id.templete1)).setText(getString(R.string.content_default));
+	            			((EditText)findViewById(R.id.templete2)).setText(getString(R.string.content_default_2));
+	            			((EditText)findViewById(R.id.templete3)).setText(getString(R.string.content_default_3));
+	            			((CheckBox)findViewById(R.id.quit_after_sharing)).setChecked(false);
+	                    }
+	                });
+	        alertDialogBuilder.setNegativeButton(getString(R.string.cancel),
+	                new DialogInterface.OnClickListener() {
+	                    @Override
+	                    public void onClick(DialogInterface dialog, int which) {
+	                    }
+	                });
+	        alertDialogBuilder.setCancelable(true);
+	        AlertDialog alertDialog = alertDialogBuilder.create();
+	        alertDialog.show();
 		}
 	}
 
